eb40eb95f9fcfc8c425d65e39faa015ecea4278b
mohammedashique <ashiqueoffl@gmail.com>
2026-05-28 14:27:49 +0530

    fix: change default server port from 5000 to 3000

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.use((err, req, res, next) => {
 connectDB();
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Running on ${PORT}`);
